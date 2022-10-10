13a4798785ac4ccbcb1064eb8e80b0d9dce48e72
Riski Agung <riski97@gmail.com>
2022-10-10 18:06:06 +0700

    update home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,159 +15,191 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+      ]),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              // header
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
                 children: [
-                  // user
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  // header
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Hi, Riski!',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      // user
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hi, Riski!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '10 Oct 2022',
+                            style: TextStyle(
+                              color: Colors.blue[200],
+                              // fontSize: 16,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 5),
-                      Text(
-                        '10 Oct 2022',
-                        style: TextStyle(
-                          color: Colors.blue[200],
-                          // fontSize: 16,
+
+                      // notif
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius: BorderRadius.circular(12)),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.white,
                         ),
                       ),
                     ],
                   ),
-
-                  // notif
+                  SizedBox(height: 20),
+                  // search bar
                   Container(
                     decoration: BoxDecoration(
                         color: Colors.blue[600],
                         borderRadius: BorderRadius.circular(12)),
-                    padding: EdgeInsets.all(12),
-                    child: Icon(
-                      Icons.notifications,
-                      color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.search,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text('Search', style: TextStyle(color: Colors.white))
+                        ],
+                      ),
                     ),
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // bad
+                      Column(
+                        children: [
+                          Emoji(
+                            emoji: '😣',
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Bad',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // fine
+                      Column(
+                        children: [
+                          Emoji(
+                            emoji: '🙂',
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Fine',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // well
+                      Column(
+                        children: [
+                          Emoji(
+                            emoji: '😀',
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Well',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      // excelent
+                      Column(
+                        children: [
+                          Emoji(
+                            emoji: '🥰',
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Excelent',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
-              SizedBox(height: 20),
-              // search bar
-              Container(
+            ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
-                    color: Colors.blue[600],
-                    borderRadius: BorderRadius.circular(12)),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text('Search', style: TextStyle(color: Colors.white))
-                    ],
-                  ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    )),
+                child: Column(
+                  children: [Text('coba')],
                 ),
               ),
-              SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'How do you feel?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.white,
-                  )
-                ],
-              ),
-              SizedBox(height: 25),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  // bad
-                  Column(
-                    children: [
-                      Emoji(
-                        emoji: '😣',
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Bad',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // fine
-                  Column(
-                    children: [
-                      Emoji(
-                        emoji: '🙂',
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Fine',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // well
-                  Column(
-                    children: [
-                      Emoji(
-                        emoji: '😀',
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Well',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  // excelent
-                  Column(
-                    children: [
-                      Emoji(
-                        emoji: '🥰',
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        'Excelent',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
