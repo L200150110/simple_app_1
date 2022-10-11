a29b882e5dfb304b936ba390db6b1d697a073762
Riski Agung <riski97@gmail.com>
2022-10-11 11:19:32 +0700

    update final home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import '../utils/emoji.dart';
+import '../utils/excercises.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,186 +22,213 @@ class _HomePageState extends State<HomePage> {
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+          icon: Icon(Icons.message),
+          label: 'Message',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
+          icon: Icon(Icons.person),
+          label: 'Profile',
         ),
       ]),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                children: [
-                  // header
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // user
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Hi, Riski!',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            '10 Oct 2022',
-                            style: TextStyle(
-                              color: Colors.blue[200],
-                              // fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      // notif
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.blue[600],
-                            borderRadius: BorderRadius.circular(12)),
-                        padding: EdgeInsets.all(12),
-                        child: Icon(
-                          Icons.notifications,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  // search bar
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.blue[600],
-                        borderRadius: BorderRadius.circular(12)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.search,
-                            color: Colors.white,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text('Search', style: TextStyle(color: Colors.white))
-                        ],
-                      ),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Column(children: [
+              // header
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                // user
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Text(
+                    'Hi, Riski!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(height: 25),
+                  SizedBox(height: 5),
+                  Text(
+                    '10 Oct 2022',
+                    style: TextStyle(
+                      color: Colors.blue[200],
+                      // fontSize: 16,
+                    ),
+                  ),
+                ]),
+
+                // notif
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: EdgeInsets.all(12),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
+                ),
+              ]),
+              SizedBox(height: 20),
+              // search bar
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue[600],
+                    borderRadius: BorderRadius.circular(12)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(children: [
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text('Search', style: TextStyle(color: Colors.white))
+                  ]),
+                ),
+              ),
+              SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'How do you feel?',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    Icons.more_horiz,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+              SizedBox(height: 25),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                // bad
+                Column(children: [
+                  Emoji(
+                    emoji: '😣',
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Bad',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+                // fine
+                Column(children: [
+                  Emoji(
+                    emoji: '🙂',
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Fine',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+                // well
+                Column(children: [
+                  Emoji(
+                    emoji: '😀',
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Well',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+                // excelent
+                Column(children: [
+                  Emoji(
+                    emoji: '🥰',
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Excelent',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ]),
+              ]),
+            ]),
+          ),
+          Expanded(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25.0, vertical: 5.0),
+                child: Column(children: [
+                  Icon(
+                    Icons.horizontal_rule,
+                    color: Colors.grey[400],
+                    size: 30,
+                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'How do you feel?',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
+                      Text('Excercises',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.grey[700],
+                          )),
+                      Icon(Icons.more_horiz, color: Colors.grey[700]),
+                    ],
+                  ),
+                  Expanded(
+                    child: ListView(
+                      children: [
+                        Excercises(
+                          icon: Icons.favorite,
+                          exercisesTitle: 'Speaking Skills',
+                          numberExercises: 15,
+                          color: Colors.red[400],
                         ),
-                      ),
-                      Icon(
-                        Icons.more_horiz,
-                        color: Colors.white,
-                      )
-                    ],
+                        Excercises(
+                          icon: Icons.person,
+                          exercisesTitle: 'Reading Skills',
+                          numberExercises: 10,
+                          color: Colors.green[400],
+                        ),
+                        Excercises(
+                          icon: Icons.star,
+                          exercisesTitle: 'Writting Skills',
+                          numberExercises: 22,
+                          color: Colors.pink[400],
+                        ),
+                        Excercises(
+                          icon: Icons.speaker,
+                          exercisesTitle: 'Listening Skills',
+                          numberExercises: 8,
+                          color: Colors.blue[400],
+                        ),
+                      ],
+                    ),
                   ),
-                  SizedBox(height: 25),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      // bad
-                      Column(
-                        children: [
-                          Emoji(
-                            emoji: '😣',
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Bad',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      // fine
-                      Column(
-                        children: [
-                          Emoji(
-                            emoji: '🙂',
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Fine',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      // well
-                      Column(
-                        children: [
-                          Emoji(
-                            emoji: '😀',
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Well',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                      // excelent
-                      Column(
-                        children: [
-                          Emoji(
-                            emoji: '🥰',
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            'Excelent',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
+                ]),
               ),
             ),
-            Expanded(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    )),
-                child: Column(
-                  children: [Text('coba')],
-                ),
-              ),
-            )
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
---
+++ lib/utils/excercises.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Excercises extends StatelessWidget {
+  final icon;
+  final String exercisesTitle;
+  final int numberExercises;
+  final color;
+
+  const Excercises({
+    Key? key,
+    required this.icon,
+    required this.exercisesTitle,
+    required this.numberExercises,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 6.0),
+      child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+            // boxShadow: BoxShadow.lerpList(0, 1, 0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: color,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        icon,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(exercisesTitle,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      const SizedBox(height: 5),
+                      Text('$numberExercises Excercises',
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          )),
+                    ],
+                  )
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Icon(Icons.more_horiz),
+              )
+            ],
+          )),
+    );
+  }
+}
