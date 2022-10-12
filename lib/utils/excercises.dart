e03cba6e69649639fb0971cc286b3d39bb46d20b
Riski Agung <riski97@gmail.com>
2022-10-12 10:33:05 +0700

    update

--- lib/utils/excercises.dart
+++ lib/utils/excercises.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 
 class Excercises extends StatelessWidget {
@@ -49,9 +51,10 @@ class Excercises extends StatelessWidget {
                   children: [
                     Text(
                       exercisesTitle,
-                      style: const TextStyle(
+                      style: TextStyle(
                         fontSize: 16,
                         fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
                       ),
                     ),
                     const SizedBox(height: 5),
